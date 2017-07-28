3f3c30f3cb1eb39f8fc772d64f93cd80c10d0649
Christian Hensen <christian.j.hensen@gmail.com>
2017-07-27 21:17:58 -0400

    separated questions into own file, object variable for keeping state of questions, function to assign event handlers

--- js/app.js
+++ js/app.js
@@ -1,101 +1,3 @@
-const QUESTIONS = [
-  {
-    question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
-    a: "William and Elizabeth",
-    b: "Joseph and Catherine",
-    c: "John and Mary",
-    d: "George and Anne",
-    answer: "c",
-    correctStatus: false
-  },
-  {
-    question: "When did the Liberty Bell get its name?",
-    a: "When it was made, in 1701",
-    b: "When it rang on July 4, 1776",
-    c: "In the 19th century, when it became a symbol of the abolition of slavery",
-    d: "None of the above",
-    answer: "c",
-    correctStatus: false
-  },
-  {
-    question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
-    a: "Home computer",
-    b: "Compact Disk Player",
-    c: "Cordless phone",
-    d: "Dishwasher",
-    answer: "b",
-    correctStatus: false
-  },
-  {
-    question: "In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?",
-    a: "8",
-    b: "18",
-    c: "38",
-    d: "58",
-    answer: "b",
-    correctStatus: false
-  },
-  {
-    question: "During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?",
-    a: "Cocker Spaniel",
-    b: "German Shepherd",
-    c: "Labrador Retriever",
-    d: "Poodle",
-    answer: "a",
-    correctStatus: false
-  },
-  {
-    question: "In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?",
-    a: "10%",
-    b: "15%",
-    c: "31%",
-    d: "51%",
-    answer: "c",
-    correctStatus: false
-  },
-  {
-    question: "The first black American pictured on a U.S. postage stamp was who?",
-    a: "Frederick Douglass",
-    b: "Booker T Washington",
-    c: "Louis Armstrong",
-    d: "Joe Louis",
-    answer: "d",
-    correctStatus: false
-  },
-  {
-    question: "What did the \"D\" in \"D-Day\" stand for?",
-    a: "doom",
-    b: "day",
-    c: "Dwight (Eisenhower)",
-    d: "Dunkirk",
-    answer: "b",
-    correctStatus: false
-  },
-  {
-    question: "Which of these characters turned 40 years old in 1990?",
-    a: "Charlie Brown",
-    b: "Bugs Bunny",
-    c: "Mickey Mouse",
-    d: "Fred Flintstone",
-    answer: "a",
-    correctStatus: false
-  },
-  {
-    question: "Before becoming George Bush's Secretary of Defense, what was Dick Cheney's position?",
-    a: "congressman from Wyoming",
-    b: "governor of New Hampshire",
-    c: "secretary of defense under Ronald Reagan",
-    d: "monkey",
-    answer: "a",
-    correctStatus: false
-  }
-];
-
-
-
-
-
-
 // START SCREEN
 // Start the game
   // Click the start button √
@@ -148,11 +50,15 @@ const START_BUTTON_CLASS = '.js-btn-start';
 const QUESTION_ELEMENT_CLASS = '.js-question';
 const QUESTION_FORM = '.js-question-answers';
 const QUESTION_TITLE_ELEMENT = `${QUESTION_ELEMENT_CLASS} h2`;
-const QUESTION_INDEX_ATTR_IDENTIFIER = 'data-question-index';
 const NEXT_BUTTON_CLASS = ".js-btn-next";
 const QUESTION_FEEDBACK_ELEMENT = '.js-question-feedback p';
 const QUESTION_COUNT_ELEMENT = '.js-question-count';
 const SCORE_COUNT_ELEMENT = '.js-score-count';
+
+const applicationState = {
+  dataQuestionIndex: 0,
+  correctQuestionStatus: []
+}
 
 
 // Hides/removes the start view when the start button is clicked
@@ -169,7 +75,7 @@ function generateQuestionTitle(questionIndex) {
 
 // Generate the HTML for the current question
 function generateQuestionElement(questionIndex) {
-  return `<div class="question-element" data-question-index="${questionIndex}">
+  return `<div class="question-element">
             <div class="row">
               <label class="col-6" for="answerChoice-0">${QUESTIONS[questionIndex].a}
                 <input type="radio" id="answerChoice-0" name="answer" value="${QUESTIONS[questionIndex].a}">
@@ -203,13 +109,13 @@ function renderQuestionElement(questionIndex) {
   // Hiding the start view
   // Showing the questions
 function handleStartButtonClicked() {
-  $(START_BUTTON_CLASS).on('click', function() {
+  // $(START_BUTTON_CLASS).on('click', function() {
     console.log('start clicked');    
     hideStartView();
-    $(QUESTION_ELEMENT_CLASS).show();
+    $(QUESTION_ELEMENT_CLASS).show(); // could re-do this with a function
     generateQuestionTitle(getQuestionIndex());
     renderQuestionElement(getQuestionIndex());
-  });
+  // });
 }
 
 // Gets the index of the current question
@@ -218,11 +124,12 @@ function handleStartButtonClicked() {
 // If it does exist, it uses the index from the data attribute
 function getQuestionIndex() {
   console.log('getQuestionIndex');
-  if($('.question-element').attr(QUESTION_INDEX_ATTR_IDENTIFIER)) {
-    return parseInt($('.question-element').attr(QUESTION_INDEX_ATTR_IDENTIFIER), 10);
-  } else {
-    return 0;
-  }
+  // if($('.question-element').attr(QUESTION_INDEX_ATTR_IDENTIFIER)) {
+  //   return parseInt($('.question-element').attr(QUESTION_INDEX_ATTR_IDENTIFIER), 10);
+  // } else {
+  //   return 0;
+  // }
+  return applicationState.dataQuestionIndex;
 }
 
 // Watch the next button for a click
@@ -230,25 +137,22 @@ function getQuestionIndex() {
 // Generate the title using the index
 // Render the question using the index
 function handleNextButtonClicked() {
-  $(NEXT_BUTTON_CLASS).on('click', function() {
-    console.log('handleNextButtonClicked');
-    const questionIndex = getQuestionIndex() + 1;
-    generateQuestionTitle(questionIndex);
-    renderQuestionElement(questionIndex);
-    disableNextButton();
-    clearFeedbackMessage();
-    setQuestionCount();
-  });
+  console.log('handleNextButtonClicked');
+  applicationState.dataQuestionIndex += 1;
+  const questionIndex = getQuestionIndex();
+  generateQuestionTitle(questionIndex);
+  renderQuestionElement(questionIndex);
+  disableNextButton();
+  clearFeedbackMessage();
+  setQuestionCount();
 }
 
 function handleAnswerChoiceClicked() {
-  $(QUESTION_FORM).on('click', 'input', function() {
-    const answer = $(this).val();
-    validateAnswer(answer);
-    disableOtherAnswers($(this));
-    setScoreCount();
-    enableNextButton();
-  });
+  const answer = $(this).val();
+  validateAnswer(answer);
+  disableOtherAnswers($(this));
+  setScoreCount();
+  enableNextButton();
 }
 
 function validateAnswer(answer) {
@@ -267,7 +171,9 @@ function validateAnswer(answer) {
 function setCorrectStatus(answer, status) {
   console.log('setCorrectStatus', answer);
   const questionIndex = getQuestionIndex();
-  QUESTIONS[questionIndex].correctStatus = status;
+  const newCorrectStatus = {};
+  newCorrectStatus['status'] = status;
+  applicationState.correctQuestionStatus.push(newCorrectStatus);
 }
 
 function updateFeedbackMessage(message) {
@@ -304,22 +210,29 @@ function setScoreCount() {
 function getScoreCount() {
   let scoreCount = 0;
   console.log('getScoreCount');
-  QUESTIONS.forEach(function(question) {
-    if(question.correctStatus === true) {
+  applicationState.correctQuestionStatus.forEach(function(question) {
+    if(question.status === true) {
       scoreCount += 1;
     }
   });
   return scoreCount;
 }
 
+function assignEventHandlers() {
+  // ? Why does this fire immediately with the ()
+  // $(START_BUTTON_CLASS).on('click', handleStartButtonClicked());
+  $(START_BUTTON_CLASS).on('click', handleStartButtonClicked);
+  $(NEXT_BUTTON_CLASS).on('click', handleNextButtonClicked);
+  $(QUESTION_FORM).on('click', 'input', handleAnswerChoiceClicked);
+}
 // Start the quiz
 // Watch for start button click
 // Watch for next button click
+// Handle answer choice
+// Set question count
 function startQuiz() {
   console.log('startQuiz');
-  handleStartButtonClicked();
-  handleNextButtonClicked();
-  handleAnswerChoiceClicked();
+  assignEventHandlers();
   setQuestionCount();
 }
 
---
+++ js/questions.js
@@ -0,0 +1,82 @@
+const QUESTIONS = [
+  {
+    question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+    a: "William and Elizabeth",
+    b: "Joseph and Catherine",
+    c: "John and Mary",
+    d: "George and Anne",
+    answer: "c"
+  },
+  {
+    question: "When did the Liberty Bell get its name?",
+    a: "When it was made, in 1701",
+    b: "When it rang on July 4, 1776",
+    c: "In the 19th century, when it became a symbol of the abolition of slavery",
+    d: "None of the above",
+    answer: "c"
+  },
+  {
+    question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+    a: "Home computer",
+    b: "Compact Disk Player",
+    c: "Cordless phone",
+    d: "Dishwasher",
+    answer: "b"
+  },
+  {
+    question: "In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?",
+    a: "8",
+    b: "18",
+    c: "38",
+    d: "58",
+    answer: "b"
+  },
+  {
+    question: "During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?",
+    a: "Cocker Spaniel",
+    b: "German Shepherd",
+    c: "Labrador Retriever",
+    d: "Poodle",
+    answer: "a"
+  },
+  {
+    question: "In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?",
+    a: "10%",
+    b: "15%",
+    c: "31%",
+    d: "51%",
+    answer: "c"
+  },
+  {
+    question: "The first black American pictured on a U.S. postage stamp was who?",
+    a: "Frederick Douglass",
+    b: "Booker T Washington",
+    c: "Louis Armstrong",
+    d: "Joe Louis",
+    answer: "d"
+  },
+  {
+    question: "What did the \"D\" in \"D-Day\" stand for?",
+    a: "doom",
+    b: "day",
+    c: "Dwight (Eisenhower)",
+    d: "Dunkirk",
+    answer: "b"
+  },
+  {
+    question: "Which of these characters turned 40 years old in 1990?",
+    a: "Charlie Brown",
+    b: "Bugs Bunny",
+    c: "Mickey Mouse",
+    d: "Fred Flintstone",
+    answer: "a"
+  },
+  {
+    question: "Before becoming George Bush's Secretary of Defense, what was Dick Cheney's position?",
+    a: "congressman from Wyoming",
+    b: "governor of New Hampshire",
+    c: "secretary of defense under Ronald Reagan",
+    d: "monkey",
+    answer: "a"
+  }
+];
